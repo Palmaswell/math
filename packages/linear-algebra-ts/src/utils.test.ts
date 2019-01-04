6ca90e62df92ff48fd21798d21a060b5bf1470ee
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2019-01-04 19:09:28 +0100

    fix(utils): degrees to radians unit test

--- packages/linear-algebra-ts/src/utils.test.ts
+++ packages/linear-algebra-ts/src/utils.test.ts
@@ -1,5 +1,6 @@
 import * as Utils from './utils';
 
 test('Radians to degrees', () => {
-  expect(Utils.radToDeg(1)).toEqual(2);
+  expect(Utils.radToDeg(150)).toEqual(8594.366926962348);
+  expect(Utils.radToDeg(-45)).toEqual(-2578.3100780887044);
 });
