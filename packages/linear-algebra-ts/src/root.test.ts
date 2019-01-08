fc4ad60a17f611e722b256608f1e4a97861b6613
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2019-01-08 23:25:25 +0100

    feat(type): implement square root function using the bisection method

--- packages/linear-algebra-ts/src/root.test.ts
+++ packages/linear-algebra-ts/src/root.test.ts
@@ -1,25 +1,32 @@
 import * as Math from './root';
 
-test('Square Root Bisection method', () => {
+test('Bisection method', () => {
   expect(Math.bisection({
-    f: Math.f(10),
+    f: Math.intermediateZero(10),
     low: -2,
     high: 5
   })).toEqual(3.162278);
   expect(Math.bisection({
-    f: Math.f(9),
+    f: Math.intermediateZero(9),
     low: -2,
     high: 6
   })).toEqual(3);
   expect(Math.bisection({
-    f: Math.f(16),
+    f: Math.intermediateZero(16),
     low: -2,
     high: 6
   })).toEqual(4);
   expect(Math.bisection({
-    f: Math.f(11),
+    f: Math.intermediateZero(11),
     low: -2,
     high: 5
   })).toEqual(3.316625);
 });
 
+test('Square Root Function', () => {
+  expect(Math.sqroot(9)).toEqual(3);
+  expect(Math.sqroot(11)).toEqual(3.316625);
+  expect(Math.sqroot(-4.587 * -4.587)).toEqual(4.587);
+  expect(Math.sqroot(0.685 * 0.685)).toEqual(0.685);
+  expect(Math.sqroot(8.444 * 8.444)).toEqual(8.444);
+})
--- packages/linear-algebra-ts/src/root.ts
+++ packages/linear-algebra-ts/src/root.ts
@@ -1,10 +1,10 @@
-
 /**
- * Bisection method
- * Root finding proplem `f(x) = 0`
- * @param f a continuous function.
- * @param a interval |a, b|
- * @param b interval |a, b|
+ * @name Bisection method
+ * @description Finding the root of an equation `f(x) = 0`
+ * Find the root of a number. Which is our equation `x² = n`
+ * @param f a continuous function `f(x) = 0`.
+ * @param low  braket interval |low, high|
+ * @param high braket interval |low, high|
  */
 
 export interface BisectionProps {
@@ -13,7 +13,6 @@ export interface BisectionProps {
   high: number;
   i?: number;
 }
-export const f = (value: number) => (n: number) => n * n - value;
 
 export const bisection = ({f, low, high, i = 0}: BisectionProps): number => {
   const tol = 1e-15;
@@ -42,3 +41,39 @@ export const bisection = ({f, low, high, i = 0}: BisectionProps): number => {
     return +mid.toFixed(6);
   }
 }
+
+/**
+ * @name The Intermediate Zero Theorem
+ * @description Let f(x) be a function which is continuous
+ * on the closed interval [a,b]. If f(a) and f(b) have
+ * opposite signs or that one is zero. `f(a)f(b) <= 0`
+ * Then there is at least one c with `a < c < b` such that f(c) = 0.
+ */
+export const intermediateZero = (value: number) => (n: number) => n ** 2 - value;
+
+/**
+ * @name sqroot
+ * @description Returns an implementation-dependent approximation
+ */
+export const sqroot = (x: number): number => {
+  if(Number.isNaN(x)) {
+    return NaN;
+  }
+  if(x < 0) {
+    return NaN;
+  }
+  if(x === 0) {
+    return 0;
+  }
+  return bisection({
+    f: intermediateZero(x),
+    low: 0,
+    high: x < 1 ? 1 : x
+  });
+};
+
+/**
+ * Newton-Raphson method
+ * @description Root finding proplem `f(x) = 0`
+ * Initial approximation guess `x0`
+ */
